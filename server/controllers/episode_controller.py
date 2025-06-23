602d74092e1ec5837d71a825a892ab19927f1b1c
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-06-23 19:30:43 +0300

    Adds data to models and controllers

---
+++ server/controllers/episode_controller.py
@@ -0,0 +1,30 @@
+from flask import Blueprint, jsonify, request
+from flask_jwt_extended import jwt_required
+from server.models import db
+
+from server.models.episode import Episode
+
+episode_bp = Blueprint("episodes", __name__)
+
+@episode_bp.route("/episodes", methods=["GET"])
+def get_episodes():
+    episodes = Episode.query.all()
+    return jsonify([{"id": e.id, "date": e.date, "number": e.number} for e in episodes])
+
+@episode_bp.route("/episodes/<int:id>", methods=["GET"])
+def get_episode(id):
+    episode = Episode.query.get_or_404(id)
+    return jsonify({
+        "id": episode.id,
+        "date": episode.date,
+        "number": episode.number,
+        "appearances": [{"guest_id": a.guest_id, "rating": a.rating} for a in episode.appearances]
+    })
+
+@episode_bp.route("/episodes/<int:id>", methods=["DELETE"])
+@jwt_required()
+def delete_episode(id):
+    episode = Episode.query.get_or_404(id)
+    db.session.delete(episode)
+    db.session.commit()
+    return jsonify({"message": "Episode deleted"})
---
+++ server/controllers/guest_controller.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, jsonify
+from server.models.guest import Guest
+from server.models import db
+
+
+guest_bp = Blueprint("guests", __name__)
+
+@guest_bp.route("/guests", methods=["GET"])
+def get_guests():
+    guests = Guest.query.all()
+    return jsonify([{"id": g.id, "name": g.name, "occupation": g.occupation} for g in guests])
